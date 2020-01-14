a8bcfa57ae62c05cd36b94a6c3d4c111b0c5a765
MoonSolid <rnreo20@gmail.com>
2020-01-14 18:17:09 +0900

    v23

--- bitcamp-java/src/test/java/Test1.java
+++ bitcamp-java/src/test/java/Test1.java
@@ -1,9 +1,129 @@
+import java.util.Arrays;
 
-public class Test1 {
-  public static void main(String[] args) {
-byte a = 10;
-byte b = 20;
-byte c = (byte)(a + b);
-System.out.println(c);
+public class Test1<E> {
+
+  private static final int DEFAULT_CAPACITY = 10;
+
+  Object[] elementData;
+  int size;
+
+
+
+
+  public Test1() {
+    this.elementData = new Object[DEFAULT_CAPACITY];
+  }
+
+
+  public Test1(int initialCapacity) {
+    if (initialCapacity < DEFAULT_CAPACITY) {
+      this.elementData = new Object[DEFAULT_CAPACITY];
+    } else {
+      this.elementData = new Object[initialCapacity];
+    }
+  }
+  public void add(E e) {
+
+    if (this.size == this.elementData.length ) {
+      grow();
+    }
+    this.elementData[this.size++] = e;
+  }
+
+  @SuppressWarnings("unchecked")
+  public E get(int index) {
+    if (index < 0 || index >= this.size) {
+      return null;
+    }
+
+    return (E) this.elementData[index];
+  }
+
+  @SuppressWarnings("unchecked")
+  public E set(int index, E e) {
+    if (index < 0 || index >= this.size ) {
+      return null;
+    }
+    E oldValue = (E) this.elementData[index];
+    this.elementData[index] = e;
+    return oldValue;    
+  }
+
+  @SuppressWarnings("unchecked")
+  public E remove(int index) {
+    if (index < 0 || index >= this.size) {
+      return null;
+    }
+
+    E oldValue = (E) this.elementData[index];
+    System.arraycopy(this.elementData, index + 1,
+        this.elementData, index, this.size - (index + 1) );
+
+
+    /*
+    for (int i = index + 1; i < this.size; i++) {
+      this.elementData[i - 1] = this.elementData[i];
+    }
+     */
+
+    this.size --;
+    return oldValue;
+  }
+
+  public int size() {
+    return this.size;
+  }
+
+  public Object[] toArray() {
+
+    return Arrays.copyOf(this.elementData, this.size);
+
+    /*
+    Object[] arr = new Object[this.size];
+    for (int i = 0; i < this.size; i++) {
+      arr[i] = this.elementData[i];
+    }
+    return arr;
+     */
+  }
+
+  @SuppressWarnings("unchecked")
+  public E[] toArray(E[] arr) {
+    if (arr.length < this.size ) {
+      return (E[]) Arrays.copyOf(this.elementData, this.size, arr.getClass());
+    }
+    System.arraycopy(this.elementData, 0, arr, 0, this.size);   
+    return arr;
+  }
+  
+  public void add(int index, E value) {
+    if (index < 0 || index >= this.size)
+      return;
+    if (this.size == this.elementData.length) {
+      
+      grow();
+    }
+    for (int i = size - 1; i >= index; i--)
+      this.elementData[i + 1] = this.elementData[i];
+    
+    this.elementData[index] = value;
+    this.size++;
+  }
+  
+  public Object[] grow() {
+   
+    return this.elementData = Arrays.copyOf(this.elementData,
+        newCapacity());
+  }
+  
+  private int newCapacity() {
+    int oldSize = this.elementData.length;
+    return oldSize + (oldSize >> 1);
   }
 }
+
+
+
+
+
+
