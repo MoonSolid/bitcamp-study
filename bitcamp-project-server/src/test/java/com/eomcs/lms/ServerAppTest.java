ea6c73a2940be67538e9cdb3999a147004303e9a
MoonSolid <rnreo20@gmail.com>
2020-02-04 18:18:27 +0900

    v32 서버 클라이언트 작업

--- bitcamp-project-server/src/test/java/com/eomcs/lms/ServerAppTest.java
+++ bitcamp-project-server/src/test/java/com/eomcs/lms/ServerAppTest.java
@@ -3,9 +3,9 @@ package com.eomcs.lms;
 import org.junit.Test;
 
 public class ServerAppTest {
+
   @Test
-  public void testAppHasAGreeting() {
-    // ServerApp classUnderTest = new ServerApp();
-    // assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+  public void testBoardList() {
+
   }
 }
